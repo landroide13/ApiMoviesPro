f3d4e4dbb95e64de9ad8ba2018442bb1b1acb582
Luis Andrade Reuss <reysh_2@yahoo.com>
2022-08-29 12:12:20 +1200

    super user

--- movieApi/admin.py
+++ movieApi/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Movie, CustomerProfile
 
 # Register your models here.
+
+admin.site.register(Movie)
+admin.site.register(CustomerProfile)
+
+
+
+
+
+
+
+
+
